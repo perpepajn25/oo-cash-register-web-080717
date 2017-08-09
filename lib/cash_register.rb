95f1214f959cbf121bdadb0f2d6a4ff7490064c6
Natalie Perpepaj <perpepajn25@gmail.com>
2017-08-09 14:51:35 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total , :discount, :items
+
+  def initialize(employee_discount = nil)
+    @total = 0
+    @discount = employee_discount
+    @items= []
+  end
+
+  def add_item(title,price,quantity=1)
+    @total += price*quantity
+    @items << ("#{title} "*quantity).split
+  end
+
+  def apply_discount
+    if @discount != nil
+      @total -= @discount*10
+      "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    @items.flatten
+  end
+
+  def void_last_transaction
+    @total -= @total
+  end
+
+end
